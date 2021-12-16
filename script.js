dc94b23b144d5a8fde5563bcb09340b65b586740
HMS-Victory <90852625+HMS-Victory@users.noreply.github.com>
2021-12-16 05:10:48 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,128 @@
-const menuBars=document.getElementById('menu-bars');
-const overlay=document.getElementById('overlay');
-const nav1=document.getElementById('nav-1');
-const nav2=document.getElementById('nav-2');
-const nav3=document.getElementById('nav-3');
-const nav4=document.getElementById('nav-4');
-const nav5=document.getElementById('nav-5');
-const navItems=[nav1, nav2, nav3, nav4, nav5];
+const inputContainer=document.getElementById('input-container');
+const countdownForm=document.getElementById('countdown-form');
+const dateEl=document.getElementById('date-picker');
 
-//Control Navigation Animation
-function navAnimation(direction1, direction2) {
-    navItems.forEach((nav, i) => {
-        nav.classList.replace(`slide-${direction1}-${i+1}`, `slide-${direction2}-${i+1}`)
-    })
+const countdownEl=document.getElementById('countdown');
+const countdownElTitle=document.getElementById('countdown-title');
+const countdownBtn=document.getElementById('countdown-button');
+const timeElements=document.querySelectorAll('span');
+
+const completeEl=document.getElementById('complete');
+const completElInfo=document.getElementById('complete-info');
+const completeBtn=document.getElementById('complete-button');
+
+let countdownTitle='';
+let countdowndate='';
+let countdownValue= Date;
+let countdownActive;
+let savedCountdown;
+
+const second=1000;
+const minute=second*60;
+const hour=minute*60;
+const day=hour*24;
+
+//Set Date input
+const today=new Date().toISOString().split('T')[0];
+dateEl.setAttribute('min', today);
+
+//Populate Countdown/Complete UI
+function updateDOM() {
+    countdownActive =setInterval(()=> {
+    const now=new Date().getTime();
+    const distance=countdownValue-now;
+
+    const days=Math.floor(distance/day);
+    const hours=Math.floor((distance % day)/hour);
+    const minutes=Math.floor((distance % hour)/minute);
+    const seconds=Math.floor((distance % minute)/second);
+
+    //Hide Input
+    inputContainer.hidden=true; 
+
+    //If the countdown has ended, show complete
+    if(distance<0) {
+        countdownEl.hidden=true;
+        clearInterval(countdownActive);
+        completElInfo.textContent=`${countdownTitle} finished on ${countdownDate}`;
+        completeEl.hidden=false;
+    }else {
+        //Else, show the countdown in progress
+        countdownElTitle.textContent=`${countdownTitle}`;
+        timeElements[0].textContent=`${days}`;
+        timeElements[1].textContent=`${hours}`;
+        timeElements[2].textContent=`${minutes}`;
+        timeElements[3].textContent=`${seconds}`;
+        completeEl.hidden=true;
+        countdownEl.hidden=false;
+    }
+}, second);
 }
-function toggleNav(){
-    //Toggle: Menu Bars Open/Closed
-    menuBars.classList.toggle('change');
-    //Toggle Menu Active
-    overlay.classList.toggle('overlay-active');
-    if (overlay.classList.contains('overlay-active')) {
-        //Animate Overlay
-        overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
-         //Animate In-Nav Items
-        navAnimation('out', 'in');
-    }else{
-        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
 
-        //Animate Out -Nav Items
-        navAnimation('in', 'out');
+//Take Values from Form Input
+function updateCountdown(e) {
+    //Check for valid date
+    e.preventDefault();
+    countdownTitle=e.srcElement[0].value;
+    countdownDate=e.srcElement[1].value;
+    savedCountdown={
+        title: countdownTitle,
+        date: countdownDate,
+    };
+    console.log(savedCountdown)
+    localStorage.setItem('countdown', JSON.stringify(savedCountdown));
+    if (countdownDate==='') {
+        alert('pleaseSelect a date for the countdown.');
+    }else {
+
+        // Get number version of current Date updateDOM
+        countdownValue=new Date(countdownDate).getTime();
+        ('countdown value:', countdownValue)
+        updateDOM();
     }
 }
-//Event Listeners
-menuBars.addEventListener('click', toggleNav);
-navItems.forEach((nav)=> {
-    nav.addEventListener('click', toggleNav)
-})
+
+//Reset All Values
+function reset() {
+    //Hide Countdowns, show input
+    countdownEl.hidden=true;
+    completeEl.hidden=true;
+    inputContainer.hidden=false;
+    //Stop the Countdown
+    clearInterval(countdownActive);
+    //Reset Values
+    countdownTitle='';
+    countdownDate='';
+}
+
+function restorePreviousCountdown() {
+    //Get Countdown for local storage if available
+    if (localStorage.getItem('countdown')){
+        inputContainer.hidden=true;
+        countdownEl.hidden=false;
+        savedCountdown=JSON.parse(localStorage.getItem('countdown'));
+        countdownTitle=savedCountdown.title;
+        countdownDate=savedCountdown.date;
+        countdownValue=new Date(countdownDate).getTime();
+        updateDOM();
+    }
+}
+
+countdownForm.addEventListener('submit', updateCountdown);
+countdownBtn.addEventListener('click', reset);
+completeBtn.addEventListener('click', reset);
+
+//On Load check local storage
+restorePreviousCountdown();
+
+//No matter what when making a website, make constants for all of the HTML elements you plan on working with.
+//This will make your code much simpler.
+
+/*When fixing an error, it Is not always about spelling the variables
+ Always check the path from which you are recieving data Here,
+  when we tried to getItem, we spelled savedCountdown wrong-
+  This was a problem because it was throwing errors about 'title' and 'date'
+   which are keys inside the object 'savedCountdown'.
+    So really the errors were'nt about spelling. 
+    So if you are certain the spelling is correct, than go straight along and
+     check its paths.*/
