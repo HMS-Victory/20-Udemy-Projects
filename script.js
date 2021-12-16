15c7c04f1c7acc179be6b74595aae19fd929a286
HMS-Victory <90852625+HMS-Victory@users.noreply.github.com>
2021-12-16 07:46:43 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,27 @@
-const menuBars=document.getElementById('menu-bars');
-const overlay=document.getElementById('overlay');
-const nav1=document.getElementById('nav-1');
-const nav2=document.getElementById('nav-2');
-const nav3=document.getElementById('nav-3');
-const nav4=document.getElementById('nav-4');
-const nav5=document.getElementById('nav-5');
-const navItems=[nav1, nav2, nav3, nav4, nav5];
+const videoElement=document.getElementById('video');
+const button=document.getElementById('button');
 
-//Control Navigation Animation
-function navAnimation(direction1, direction2) {
-    navItems.forEach((nav, i) => {
-        nav.classList.replace(`slide-${direction1}-${i+1}`, `slide-${direction2}-${i+1}`)
-    })
-}
-function toggleNav(){
-    //Toggle: Menu Bars Open/Closed
-    menuBars.classList.toggle('change');
-    //Toggle Menu Active
-    overlay.classList.toggle('overlay-active');
-    if (overlay.classList.contains('overlay-active')) {
-        //Animate Overlay
-        overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
-         //Animate In-Nav Items
-        navAnimation('out', 'in');
-    }else{
-        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
-
-        //Animate Out -Nav Items
-        navAnimation('in', 'out');
+//Prompt user to select media sream, 
+async function selectMediaStream(){
+    try {
+       const mediaStream=await navigator.mediaDevices.getDisplayMedia();
+       videoElement.srcObject=mediaStream;
+       videoElement.onloadedmetadata=()=> {
+           videoElement.play();
+       }
+    }catch (error) {
+        //Catch error here
     }
 }
-//Event Listeners
-menuBars.addEventListener('click', toggleNav);
-navItems.forEach((nav)=> {
-    nav.addEventListener('click', toggleNav)
-})
+
+button.addEventListener('click', async ()=> {
+    //Disable Button
+    button.disabled=true;
+    //Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    //Reset Button
+    button.disabled=false;
+});
+
+//On Load
+selectMediaStream();
